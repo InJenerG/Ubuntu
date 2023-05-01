44a7439720872347f6a13d33952ae08d8f1110ea
Yevhenii <93005185+InJenerG@users.noreply.github.com>
2023-05-01 16:06:37 +0300

    Update install_docker_ubuntu.sh

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,10 +1,7 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
- sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg
- sudo install -m 0755 -d /etc/apt/keyrings
+ sudo apt-get install ca-certificates curl gnupg -y
+ sudo install -m 0755 -d /etc/apt/keyrings -y
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
  sudo chmod a+r /etc/apt/keyrings/docker.gpg
  echo \
@@ -12,7 +9,7 @@ sudo apt-get update
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
  sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
  sudo docker run hello-world
  sudo docker rm -vf $(docker ps -aq)
  sudo docker rmi -f $(docker images -aq)
